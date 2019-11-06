9d348294f1e1a89aa4f91df1aaef4b8df8e9ffa1
Agkdesign <anthonykamil@hotmail.com>
2019-11-06 16:48:19 +0000

    Update forge.bim.issues.report.js

--- www/js/forge.bim.issues.report.js
+++ www/js/forge.bim.issues.report.js
@@ -214,7 +214,7 @@ function refreshIssuebyCompany(company){
   };
 
 
-  var canvas = document.getElementById('issueByType');
+  var canvas = document.getElementById('issueByCompany');
   var ctx = canvas.getContext('2d'); 
   
   issueByCompanyBar = new Chart(ctx, {
